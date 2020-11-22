ee1794ee5691fc5699f421cbfc47b779dd317e22
Eymen Uraş <eymenuras123@gmail.com>
2020-11-22 20:15:52 +0300

    9

---
+++ Radyo_CSharp/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Radyo_CSharp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = "http://turkmedya.radyotvonline.com/turkmedya/alemfm.stream/playlist.m3u8";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = "http://46.20.7.125/listen.pls";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = "http://45.32.154.169:9300/;";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = "https://playerservices.streamtheworld.com/api/livestream-redirect/JOY_FM.mp3";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = "https://playerservices.streamtheworld.com/api/livestream-redirect/JOY_TURK.mp3";
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = "https://ssldyg.radyotvonline.com/kralweb/smil:kralpopstar.smil/playlist.m3u8";
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = "http://nr1digitalsc.radyotvonline.com/stream/374/";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = "http://shoutcast.radyogrup.com:1010/";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = "http://shoutcast.radyogrup.com:1020/";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = "http://provisioning.streamtheworld.com/pls/POP90AAC.pls";
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = "http://powerfm.listenpowerapp.com/powerfm/mpeg/icecast.audio";
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = "http://mpegpowerturk.listenpowerapp.com/powerturk/mpeg/icecast.audio";
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = "http://shoutcast.radyogrup.com:2020/stream/1";
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = "http://provisioning.streamtheworld.com/pls/SUPER_FMAAC.pls";
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = "http://46.20.3.228/listen.pls";
+        }
+
+        private void button6_Click_1(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = "http://www.olayfm.com.tr/live/listen.asx";
+        }
+
+        private void button9_Click_1(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            form2.Show();
+        }
+
+        private void button6_Click_2(object sender, EventArgs e)
+        {
+            Form3 form3 = new Form3();
+            form3.Show();
+        }
+    }
+}
